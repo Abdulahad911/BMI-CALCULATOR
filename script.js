f9d86983efd2ce40f09a75c25288355d19f371d6
Abdulahad911 <a.a.khan6204@gmail.com>
2024-10-08 11:31:36 -0700

    Trimming value of input if someone enter empty value display alert

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function calculatingBmi(){
     var weightInput = document.getElementById('weightInput');
     var heightInput = document.getElementById('heightInput');
-    
+    var bmi = weightInput.value/(heightInput*heightInput)
+    if (weightInput.value.trim() === '' || heightInput.value.trim() === ''){
+        return alert("Input text can't be empty")
+    }
 }
