6a701c14d7a8f1605dc9702dcb5a53f03100c8f0
Abdulahad911 <a.a.khan6204@gmail.com>
2024-10-09 00:47:47 -0700

    adding conitions for BMI

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 function calculatingBmi() {
-    var weightInput = document.getElementById('weightInput').value;
+    var weightInput = document.getElementById('weightInput').value
     var heightInput = document.getElementById('heightInput').value;
     var output = document.getElementById('output');
     if (heightInput.trim() === "" || isNaN(heightInput)) {
-        output.textContent = "Provide a valid Height!";
+      return  output.textContent = "Provide a valid Height!";
     }
 
     else if (weightInput.trim() === "" || isNaN(weightInput)) {
-        output.textContent = "Provide a valid Weight!";
+        return  output.textContent = "Provide a valid Weight!";
     }
+    Number(weightInput)
+    Number(heightInput)
     var bmi = weightInput / (heightInput * heightInput)
-
+    if (bmi < 0){
+        output.textContent = "BMI can't be negative"
+       }
     if (bmi < 18.5){
-        output.textContent = 'hhhhh'
+      return  output.textContent = `Your BMI : ${bmi.toFixed(2)} you are Under Weight`
     }
-
+   else if (bmi >= 18.6 && bmi < 24.9){
+     return output.textContent = `Your BMI : ${bmi.toFixed(2)} your weight is Normal`
+   }
+   else if (25 <= bmi && bmi < 29.9){
+     return output.textContent = `Your BMI : ${bmi.toFixed(2)} you are Overweighted`
+   }
+  
+   else {
+    return output.textContent = `Your BMI : ${bmi.toFixed(2)} your weight is Obesity`
+   }
 }
