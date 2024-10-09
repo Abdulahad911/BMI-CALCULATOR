0124942ea9715f78d22ae036887832307073609b
Abdulahad911 <a.a.khan6204@gmail.com>
2024-10-09 00:10:10 -0700

    If user give invalid value showing an alert

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function calculatingBmi(){
-    var weightInput = document.getElementById('weightInput');
-    var heightInput = document.getElementById('heightInput');
-    var bmi = weightInput.value/(heightInput*heightInput)
-    if (weightInput.value.trim() === '' || heightInput.value.trim() === ''){
-        return alert("Input text can't be empty")
+function calculatingBmi() {
+    var weightInput = document.getElementById('weightInput').value;
+    var heightInput = document.getElementById('heightInput').value;
+    var output = document.getElementById('output');
+    if (heightInput.trim() === "" || isNaN(heightInput)) {
+        output.textContent = "Provide a valid Height!";
     }
+
+    else if (weightInput.trim() === "" || isNaN(weightInput)) {
+        output.textContent = "Provide a valid Weight!";
+    }
+    var bmi = weightInput / (heightInput * heightInput)
+
+    if (bmi < 18.5){
+        output.textContent = 'hhhhh'
+    }
+
 }
